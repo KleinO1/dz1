771c18c300e0e7631e14dd85a4aab07421100cae
Pavel <pasha.mysyagin@gmail.com>
2026-05-03 18:42:24 +0300

    Первая версия

---
+++ src/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <queue>
+#include <thread>
+#include <vector>
+#include <mutex>
+
+void rabotaPotoka(int id, std::queue<int>& ochered, std::mutex& mtx) {
+    while (true) {
+        int zadacha = 0;
+
+        {
+            std::lock_guard<std::mutex> lock(mtx);
+
+            if (ochered.empty()) {
+                break;
+            }
+
+            zadacha = ochered.front();
+            ochered.pop();
+        }
+
+        std::cout << "Поток " << id << " обработал задачу "
+                  << zadacha << std::endl;
+    }
+}
+
+int main() {
+    std::queue<int> ochered;
+    std::mutex mtx;
+    std::vector<std::thread> potoki;
+
+    for (int i = 1; i <= 10; ++i) {
+        ochered.push(i);
+    }
+
+    for (int i = 1; i <= 3; ++i) {
+        potoki.push_back(std::thread(rabotaPotoka, i,
+                                     std::ref(ochered),
+                                     std::ref(mtx)));
+    }
+
+    for (std::size_t i = 0; i < potoki.size(); ++i) {
+        potoki[i].join();
+    }
+
+    std::cout << "Все потоки завершили работу" << std::endl;
+
+    return 0;
+}
